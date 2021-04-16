4f8eea68e9b65ec44f813e29b5317608d4971ebf
Gaston L Huillier <glhuilli@gmail.com>
2021-04-16 14:38:29 -0700

    Consider command line tool option "stats" as flag instead of string

--- newvelles/__main__.py
+++ newvelles/__main__.py
@@ -4,7 +4,7 @@ import click
 @click.command()
 @click.option('--limit', default=10, help='Top N news')
 @click.option('--query', default='', help='Limit news to a particular query')
-@click.option('--stats', default=False, help='Add stats for each news article')
+@click.option('--stats', is_flag=True, help='Add stats for each news article')
 def main(limit, query, stats):
     print(f'fetch news...{limit} {query} {stats}')
 
