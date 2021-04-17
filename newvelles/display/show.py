fa41ebfc10dae41735844d20b4338ff00a0d1fc3
Gaston L'Huillier <glhuilli@gmail.com>
2021-04-17 14:18:49 -0700

    Change display and logging strategy (#2)

---
+++ newvelles/display/show.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from newvelles.utils.text import group_sentences, load_embedding_model
+
+
+_EMBEDDING_MODEL = load_embedding_model()
+
+
+def print_sorted_grouped_titles(sentences: List[str], stats: bool) -> None:
+    unique_sets = group_sentences(_EMBEDDING_MODEL, sentences)
+    print(unique_sets)
+    total_groups = 0
+    for s in sorted([x for x in unique_sets if len(x) > 1], key=lambda x: len(x), reverse=True):
+        print('*' * 100)
+        total_groups += 1
+        for i in s:
+            print(sentences[i])
+    if stats:
+        print(f'total groups: {total_groups}')
--- newvelles/feed/load.py
+++ newvelles/feed/load.py
@@ -7,5 +7,11 @@ def load_paths() -> List[str]:  # pragma: no cover
         'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml',
         'https://www.huffpost.com/section/front-page/feed',
         'https://feeds.npr.org/1001/rss.xml',
-        'http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml'
+        'http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml',
+        'https://rss.politico.com/economy.xml',
+        'https://rss.politico.com/healthcare.xml',
+        'https://rss.politico.com/congress.xml',
+        'https://rss.politico.com/defense.xml',
+        'https://rss.politico.com/energy.xml',
+        'http://rss.cnn.com/rss/cnn_topstories.rss'
     ]
