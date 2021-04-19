405aec5ae8fc7c0af9d0d4b847150221d25f0cae
Gaston L'Huillier <glhuilli@gmail.com>
2021-04-19 01:13:42 -0700

    Revert "Improve sorting/grouping strategy (#3)"
    This reverts commit 2e9dad7351dca1c677a8984c124523f4418dac72.

--- newvelles/display/show.py
+++ newvelles/display/show.py
@@ -8,6 +8,7 @@ _EMBEDDING_MODEL = load_embedding_model()
 
 def print_sorted_grouped_titles(sentences: List[str], stats: bool) -> None:
     unique_sets = group_sentences(_EMBEDDING_MODEL, sentences)
+    print(unique_sets)
     total_groups = 0
     for s in sorted([x for x in unique_sets if len(x) > 1], key=lambda x: len(x), reverse=True):
         print('*' * 100)
--- newvelles/feed/log.py
+++ newvelles/feed/log.py
@@ -9,7 +9,6 @@ _LOG_NAME = 'all_entries'
 
 
 def log_entries(entries: Dict[str, List[Any]], output_path: str = _LOG_PATH) -> None:
-    log_path = f"{output_path}/{_LOG_NAME}_{'.'.join(datetime.now().isoformat().split('.')[:-1])}.jsons"
+    log_path = f"{output_path}/{_LOG_NAME}_{'.'.join(datetime.now().isoformat().split('.')[:-1])}.json'"
     with open(log_path, 'w') as f:
-        for feed, entries in entries.items():
-            f.write(json.dumps({'feed': feed, 'entries': entries})+'\n')
+        json.dump(entries, f)
