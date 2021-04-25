b1d03a53e1a44c2667d2e3e2c457e50e9cd326c0
Gaston L Huillier <glhuilli@gmail.com>
2021-04-24 21:02:31 -0700

    Load from file

--- newvelles/feed/load.py
+++ newvelles/feed/load.py
@@ -1,17 +1,7 @@
 from typing import List
 
 
-def load_paths() -> List[str]:  # pragma: no cover
-    return [
-        'https://finance.yahoo.com/rss/',
-        'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml',
-        'https://www.huffpost.com/section/front-page/feed',
-        'https://feeds.npr.org/1001/rss.xml',
-        'http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml',
-        'https://rss.politico.com/economy.xml',
-        'https://rss.politico.com/healthcare.xml',
-        'https://rss.politico.com/congress.xml',
-        'https://rss.politico.com/defense.xml',
-        'https://rss.politico.com/energy.xml',
-        'http://rss.cnn.com/rss/cnn_topstories.rss'
-    ]
+def load_rss(rss_file_path) -> List[str]:  # pragma: no cover
+    with open(rss_file_path, 'r') as f:
+        for line in f.readlines():
+            yield line.strip()
--- newvelles/feed/parser.py
+++ newvelles/feed/parser.py
@@ -7,5 +7,6 @@ def parse_feed(feed_paths: List[str]) -> Iterable[Tuple[str, Any]]:  # pragma: n
     for feed_path in feed_paths:
         feed = feedparser.parse(feed_path)
         feed_title = feed['feed']['title']
+        print(f'{feed_title} entries: {len(feed.entries)}')
         for entry in feed.entries:
             yield (feed_title, entry)
