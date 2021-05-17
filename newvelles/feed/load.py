4cbf3610e98eb783134b23535559b134df10edf1
Gaston L Huillier <glhuilli@gmail.com>
2021-05-16 18:14:14 -0700

    Updated Event data

--- newvelles/feed/load.py
+++ newvelles/feed/load.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List
 
 from newvelles.feed.log import log_entries
 from newvelles.feed.parser import parse_feed
+from newvelles.feed import NewsEntry
 
 
 def load_rss(rss_file_path) -> List[str]:  # pragma: no cover
@@ -16,7 +17,7 @@ def load_rss(rss_file_path) -> List[str]:  # pragma: no cover
             yield line.strip()
 
 
-def build_data_from_rss_feeds(rss_file: str) -> Dict[str, str]:
+def build_data_from_rss_feeds(rss_file: str) -> Dict[str, NewsEntry]:
     """
     Builds from RSS feeds, which is basically {title: link} for now.
 
@@ -26,7 +27,16 @@ def build_data_from_rss_feeds(rss_file: str) -> Dict[str, str]:
     title_data = {}
     news_data: Dict[str, Any] = defaultdict(list)
     for feed_title, entry in parse_feed(feeds):
-        title_data[entry.title] = entry.link
-        news_data[feed_title].append(entry)
+        try:
+            news_entry = NewsEntry(
+                title=entry.title,
+                link=entry.link,
+                published=entry.published,
+                title_detail_base=entry.title_detail.base,
+            )
+            title_data[entry.title] = news_entry
+            news_data[feed_title].append(entry)
+        except Exception:
+            continue
     log_entries(news_data)
     return title_data
