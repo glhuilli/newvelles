8f4fea72b1b9c1ebf3bb383046f131df3cbc3a8f
Gaston L'Huillier <glhuilli@gmail.com>
2021-05-25 05:32:10 -0700

    Catch exception when parser fails (#8)

--- newvelles/feed/parser.py
+++ newvelles/feed/parser.py
@@ -1,10 +1,12 @@
 from typing import Any, Iterable, List, Tuple
 
+import logging
 import feedparser
 
 from newvelles.config import debug
 
 DEBUG = debug()
+logger = logging.getLogger(__name__)
 
 
 def parse_feed(feed_paths: List[str]) -> Iterable[Tuple[str, Any]]:  # pragma: no cover
@@ -12,9 +14,12 @@ def parse_feed(feed_paths: List[str]) -> Iterable[Tuple[str, Any]]:  # pragma: n
     Use the feedparser package to load data from a particular RSS feed path
     """
     for feed_path in feed_paths:
-        feed = feedparser.parse(feed_path)
-        feed_title = feed['feed']['title']
-        if DEBUG:
-            print(f'{feed_title} entries: {len(feed.entries)}')
-        for entry in feed.entries:
-            yield (feed_title, entry)
+        try:
+            feed = feedparser.parse(feed_path)
+            feed_title = feed['feed']['title']
+            if DEBUG:
+                print(f'{feed_title} entries: {len(feed.entries)}')
+            for entry in feed.entries:
+                yield (feed_title, entry)
+        except Exception as e:
+            logger.exception(f'Feed path is raising exceptions: {feed_path}\nException: {str(e)}')
