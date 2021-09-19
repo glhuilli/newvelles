5755042b0e6192a73998bb09c0750ed7119bb993
Gaston L Huillier <glhuilli@gmail.com>
2021-09-19 02:02:59 -0700

    Add log to S3 for metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
       name='newvelles',
-      version='0.1.0',
+      version='0.1.1',
       description='Command line tool fetch news with a Twist.',
       url='https://github.com/glhuilli/newvelles',
       author="Gaston L'Huillier",
